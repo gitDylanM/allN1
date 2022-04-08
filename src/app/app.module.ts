262af40f0687b5db0d3e66c7f7f6d1fc8b4f92e6
Dylan McCreary <bmccreary3525@gmail.com>
2022-04-08 16:53:58 -0500

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import { SiteCheckerComponent } from './site-checker/site-checker.component';
 import { WebScraperComponent } from './web-scraper/web-scraper.component';
 import { IpLookupComponent } from './ip-lookup/ip-lookup.component';
@@ -11,7 +10,6 @@ import { IpLookupComponent } from './ip-lookup/ip-lookup.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     SiteCheckerComponent,
     WebScraperComponent,
     IpLookupComponent
